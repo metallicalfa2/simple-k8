869c990e1682c9785bd836ec3a56d3593c57de6a
rathishubham7 <shubhamrathi@icloud.com>
2019-08-30 14:40:03 +0800

    ci

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t metallicalfa/multi-client:latest -t metallicalfa/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t metallicalfa/multi-server:latest -t metallicalfa/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t metallicalfa/multi-worker:latest -t metallicalfa/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push metallicalfa/multi-client:latest
+docker push metallicalfa/multi-server:latest
+docker push metallicalfa/multi-worker:latest
+
+docker push metallicalfa/multi-client:$SHA
+docker push metallicalfa/multi-server:$SHA
+docker push metallicalfa/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=metallicalfa/multi-server:$SHA
+kubectl set image deployments/client-deployment client=metallicalfa/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=metallicalfa/multi-worker:$SHA
